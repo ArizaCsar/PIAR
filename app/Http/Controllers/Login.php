965ebd806b444d000e71880da73996df052b5bbd
Yeison <yeison.restrepo.r@gmail.com>
2020-09-30 19:57:22 -0500

    Finalización del método de autenticacion

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -3,16 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Terceros;
+use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class Login extends Controller
 {
     function doLogin(Request $request) {
 
-        $resultTercero = Terceros::where([
-            ['nombreTercero', $request->nombreTercero],
-            ['contrasena', $request->contrasena]
-        ])->firstOrFail();
-        dd($resultTercero);
+        // Obtiene el tercero con el nombre ingresado
+        $resultTercero = Terceros::where(
+            'nombreTercero', $request->nombreTercero
+        )->first();
+
+        if( !$resultTercero ) {
+            return response()->json($this->_userNotFound(), 401);
+        }
+
+        if(Hash::check($request->contrasena, $resultTercero->contrasena)) {
+            return response()->json($this->_getLoggedUser($resultTercero), 200);
+        } else {
+            return response()->json($this->_userNotFound(), 401);
+        }
+    }
+
+    private function _userNotFound() {
+        return [
+            "message" => "Usuario o contraseña incorrectos",
+            "status" => 401
+        ];
+    }
+
+    private function _getLoggedUser(Terceros $tercero) {
+
+        $now = new DateTime();
+        $token = Hash::make($now->getTimeStamp());
+
+        // crea la session con la info del token
+        session(['session_token' => $token, 'user' => $tercero]);
+
+        return [
+            "token" => session('session_token'),
+            "user" => $tercero
+        ];
     }
 }
--- app/Terceros.php
+++ app/Terceros.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Terceros extends Model
 {
-    //
+    protected $hidden = array('contrasena');
 }
